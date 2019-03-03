cd2500233068bff7ab05ff19f60218ecf2045154
Wojciech Januszek <wojsamjan@gmail.com>
2019-03-03 11:05:06 +0100

    Added validation from object duplication

--- lab2/simple_db.js
+++ lab2/simple_db.js
@@ -1,17 +1,33 @@
 var dogs = []
 
-// var erna = {
-//   name: 'Erna',
-//   breed: 'German Shepherd',
-//   age: 11,
-//   snacks: ['beef', 'chicken', 'groats'],
-//   isMale: true,
+var erna = {
+  name: 'Erna',
+  breed: 'German Shepherd',
+  age: 11,
+  snacks: ['beef', 'chicken', 'groats'],
+  isMale: true,
+}
+
+// function Dog(name, breed, age, snakcs, isMale) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.age = age;
+//   this.snakcs = snakcs;
+//   this.isMale = isMale;
 // }
 
-function Dog(name, breed, age, snakcs, isMale) {
-  this.name = name;
-  this.breed = breed;
-  this.age = age;
-  this.snakcs = snakcs;
-  this.isMale = isMale;
+// function createDog(name, breed, age, snacks, isMale) {
+//   dogs.push(new Dog('Erna', 'German Shepherd', 11, ['beef', 'groats'], false));
+// }
+
+function createDog(newDog) {
+  var found = dogs.some(function(dog) {
+    return dog.name === newDog.name;
+  });
+  if (!found) { dogs.push(newDog); }
+  else { (console.log('Can not add a dog that already exists!')); }
 }
+
+createDog(erna);
+createDog(erna);
+console.log(dogs);
