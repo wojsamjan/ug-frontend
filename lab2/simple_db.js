5adf0c0784e9592b2193035796c52fb78cb2bd0d
Wojciech Januszek <wojsamjan@gmail.com>
2019-03-03 10:36:30 +0100

    Added Dog constructor

--- lab2/simple_db.js
+++ lab2/simple_db.js
@@ -1,9 +1,17 @@
 var dogs = []
 
-var erna = {
-  name: 'Erna',
-  breed: 'German Shepherd',
-  age: 11,
-  snacks: ['beef', 'chicken', 'groats'],
-  isMale: true,
+// var erna = {
+//   name: 'Erna',
+//   breed: 'German Shepherd',
+//   age: 11,
+//   snacks: ['beef', 'chicken', 'groats'],
+//   isMale: true,
+// }
+
+function Dog(name, breed, age, snakcs, isMale) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.snakcs = snakcs;
+  this.isMale = isMale;
 }
